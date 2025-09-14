ff9aa05dd5ed35ebea4dea7f6a721f8e5ff98c97
motoko <dourval.clement@gmail.com>
2025-09-14 20:20:52 +0200

    feat(cli): setting up logging

--- src/devopso/cli.py
+++ src/devopso/cli.py
@@ -1,6 +1,18 @@
+import logging
+import logging.config
+
+from devopso.core.configuration import Configuration
+
+_LOGGING_CONFIGURATION = "resources/configs/logging.yml"
+_APP_LOGGER_NAME = "devops-overseer"
+
+
 def get_hello_string() -> str:
     return "The overseer is in the room"
 
 
 def main():
-    print(get_hello_string())
+    logging.config.dictConfig(
+        Configuration.read_configuration(_LOGGING_CONFIGURATION, expand_strs=True)
+    )
+    logging.getLogger(_APP_LOGGER_NAME).info(get_hello_string())
