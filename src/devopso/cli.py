38c557d80f5ede931b225f41e3f3b3aa8798fbdf
motoko <dourval.clement@gmail.com>
2025-09-14 20:32:42 +0200

    feat(cli): adding banner to cli launch

--- src/devopso/cli.py
+++ src/devopso/cli.py
@@ -1,17 +1,26 @@
 import logging
 import logging.config
+from importlib.resources import files
+from pathlib import Path
 
 from devopso.core.configuration import Configuration
 
 _LOGGING_CONFIGURATION = "resources/configs/logging.yml"
 _APP_LOGGER_NAME = "devops-overseer"
+_BANNER_PATH = "resources/devops-overseer.banner"
 
 
 def get_hello_string() -> str:
     return "The overseer is in the room"
 
 
+def print_banner() -> None:
+    content = Path(files("devopso").joinpath(_BANNER_PATH)).read_text(encoding="utf-8")
+    print(content)
+
+
 def main():
+    print_banner()
     logging.config.dictConfig(
         Configuration.read_configuration(_LOGGING_CONFIGURATION, expand_strs=True)
     )
