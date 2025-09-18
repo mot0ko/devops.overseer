070516d9e901d8c72bfd5ab4db05094cb5f47777
mot0ko <dourval.clement@gmail.com>
2025-09-18 18:26:44 +0200

    refactor: removing literals from the code

--- src/devopso/clients/confluence_cloud/client.py
+++ src/devopso/clients/confluence_cloud/client.py
@@ -4,9 +4,12 @@ from devopso.core.rest_client import RestClient
 
 
 class ConfluenceCloud(RestClient):
+    _CONFLUENCE_CLOUD_CONFIGURATION = "resources/configs/clients/confluence-cloud.yml"
+    _ENDPOINT_MYSELF = "myself"
+
     def __init__(self) -> None:
-        super().__init__("resources/configs/clients/confluence-cloud.yml")
+        super().__init__(ConfluenceCloud._CONFLUENCE_CLOUD_CONFIGURATION)
 
     @staticmethod
     def get_myself() -> Dict[str, Any]:
-        return ConfluenceCloud().get("myself")
+        return ConfluenceCloud().get(ConfluenceCloud._ENDPOINT_MYSELF)
